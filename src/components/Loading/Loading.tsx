217e33a04a722326286ab0274113d43031c9aff3
Saidazizhon <asrarovs26@gmail.com>
2025-01-20 01:50:09 +0500

    tugadi

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,23 @@
+import "./Loading.css"
+
+const Loading = () => {
+  return (
+<div className="pl">
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__dot"></div>
+	<div className="pl__text">Loading…</div>
+</div>
+  )
+}
+
+export default Loading
